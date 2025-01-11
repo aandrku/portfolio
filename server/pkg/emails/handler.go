afaf9e1d40c56fb7568b0f6bdf20efe055646ff8
Andrii Sozonik <aandrku@Andriis-MacBook-Air.local>
2025-01-10 19:34:12 -0800

    add: email validation

--- server/pkg/emails/handler.go
+++ server/pkg/emails/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"net/mail"
 )
 
 func EmailHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +20,11 @@ func EmailHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid body", http.StatusBadRequest)
 		return
 	}
-	// TODO: Verify valid email address.
+
+	if _, err := mail.ParseAddress(contactMsg.Email); err != nil {
+		http.Error(w, "Please provide a valid email address.", http.StatusBadRequest)
+		return
+	}
 
 	if err = Send(contactMsg.Name, contactMsg.Email, contactMsg.Message); err != nil {
 		http.Error(w, "Failed to send message. Please try again later.", http.StatusInternalServerError)
